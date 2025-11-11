74a72fca0a37d71e1087eb657aa04342c90eb563
Luis Abreu <luishdasilvaa335@gmail.com>
2025-11-10 23:13:31 -0300

    Ajustes tabela empreendimento

--- repositories/projeto_imovel_repository.py
+++ repositories/projeto_imovel_repository.py
@@ -9,19 +9,19 @@ class ProjetoImovelRepository:
         cursor = self.conn.cursor()
         try:
             query = """
-                INSERT INTO PROJETO_IMOVEL (NRO_REGISTRO, ID_IMOVEL)
+                INSERT INTO PROJETO_EMPREENDIMENTO (NRO_REGISTRO, ID_EMPREENDIMENTO)
                 VALUES (%s, %s)
             """
             cursor.execute(query, (nro_registro, id_imovel))
         except Exception as e:
-            print(f"Erro ao inserir relacionamento para NRO_REGISTRO {nro_registro} e ID_IMOVEL {id_imovel}: {e}")
+            print(f"Erro ao inserir relacionamento para NRO_REGISTRO {nro_registro} e ID_EMPREENDIMENTO {id_imovel}: {e}")
         finally:
             cursor.close()
 
     def count(self):
         """Conta o total de registros na tabela de relacionamento."""
         cursor = self.conn.cursor()
-        cursor.execute("SELECT COUNT(*) FROM PROJETO_IMOVEL")
+        cursor.execute("SELECT COUNT(*) FROM PROJETO_EMPREENDIMENTO")
         total = cursor.fetchone()[0]
         cursor.close()
         return total
